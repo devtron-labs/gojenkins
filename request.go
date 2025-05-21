566c5b2da830993e361e8bcc7cf93451c8693a5c
Prakash Kumar <prakash.kumar@devtron.ai>
2025-05-21 19:06:47 +0530

    DumpRequest and DumpResp

--- request.go
+++ request.go
@@ -138,7 +138,7 @@ func (r *Requester) SetClient(client *http.Client) *Requester {
 	return r
 }
 
-//Add auth on redirect if required.
+// Add auth on redirect if required.
 func (r *Requester) redirectPolicyFunc(req *http.Request, via []*http.Request) error {
 	if r.BasicAuth != nil {
 		req.SetBasicAuth(r.BasicAuth.Username, r.BasicAuth.Password)
@@ -226,10 +226,19 @@ func (r *Requester) Do(ctx context.Context, ar *APIRequest, responseStruct inter
 	for k := range ar.Headers {
 		req.Header.Add(k, ar.Headers.Get(k))
 	}
-
+	reqDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Request Dump: %q\n", reqDump)
 	if response, err := r.Client.Do(req); err != nil {
 		return nil, err
 	} else {
+		responseDump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Response Dump: %q\n", responseDump)
 		if v := ctx.Value("debug"); v != nil {
 			dump, err := httputil.DumpResponse(response, true)
 			if err != nil {

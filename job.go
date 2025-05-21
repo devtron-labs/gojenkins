6fc84e4b21ebf31ec61cef299e183f8612845e9c
Prakash Kumar <prakash.kumar@devtron.ai>
2025-05-21 19:31:14 +0530

    revert

--- job.go
+++ job.go
@@ -127,7 +127,7 @@ func (j *Job) GetBuild(ctx context.Context, id int64) (*Build, error) {
 		return nil, err
 	}
 	jobURL := url.Path
-	build := Build{Jenkins: j.Jenkins, Job: j, Raw: new(BuildResponse), Depth: 1, Base: jobURL + strconv.FormatInt(id, 10)}
+	build := Build{Jenkins: j.Jenkins, Job: j, Raw: new(BuildResponse), Depth: 1, Base: jobURL + "/" + strconv.FormatInt(id, 10)}
 	status, err := build.Poll(ctx)
 	if err != nil {
 		return nil, err
